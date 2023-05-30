526f2e261ab692aca7e05fed818535af5a3bee66
AlexeiZenin <zalexei7@gmail.com>
2023-05-30 15:24:07 -0400

    Update with new data + locust

--- rest/demo-rest-spring3/locustfile.py
+++ rest/demo-rest-spring3/locustfile.py
@@ -3,6 +3,14 @@ from locust import HttpUser, task, constant_throughput
 class MyUser(HttpUser):
     wait_time = constant_throughput(0.1)
 
-    @task
-    def my_task(self):
-        self.client.get("/customers")
+    # @task(1)
+    # def get_all(self):
+    #     self.client.get("/customers")
+
+    @task(1)
+    def create_customer(self):
+        self.client.post("/customers", json={"name": "Locust User", "address": "123 Main St"})
+
+    @task(2)
+    def get_customer(self):
+        self.client.get("/customer?name=John%20Smith")
